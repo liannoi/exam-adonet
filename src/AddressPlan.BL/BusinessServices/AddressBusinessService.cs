13129d17940a134e21d962b3b3cf002a6e14b321
Maksym Liannoi <liannoi444@gmail.com>
2019-10-10 19:09:42 +0300

    Implemented the principle of single responsibility
    - Removed the "divine" class
    - Introduced a base class that initializes database services. The class is abstract, and all other business services should be its heirs

---
+++ src/AddressPlan.BL/BusinessServices/AddressBusinessService.cs
@@ -0,0 +1,30 @@
+﻿using AddressPlan.BL.BusinessObjects;
+using AddressPlan.DAL.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace AddressPlan.BL.BusinessServices
+{
+    public sealed class AddressBusinessService : BaseBusinessService
+    {
+        public AddressBusinessService() : base(false)
+        {
+        }
+
+        public IEnumerable<AddressBusinessObject> GetAddresses(int streetId, int subdivisionId)
+        {
+            const string query = "EXEC dbo.GetData @StreetId, @SubdivisionId";
+
+            SqlParameter pStreetId = streetId == 0
+                ? new SqlParameter("@StreetId", DBNull.Value)
+                : new SqlParameter("@StreetId", streetId);
+
+            SqlParameter pSubdivisionId = subdivisionId == 0
+                ? new SqlParameter("@SubdivisionId", DBNull.Value)
+                : new SqlParameter("@SubdivisionId", subdivisionId);
+
+            return DataServices.Unit.Streets.GetEntities<AddressBusinessObject>(query, pStreetId, pSubdivisionId);
+        }
+    }
+}
---
+++ src/AddressPlan.BL/BusinessServices/SubdivisionBusinessService.cs
@@ -0,0 +1,37 @@
+﻿using AddressPlan.BL.BusinessObjects;
+using AddressPlan.DAL.DataServices;
+using System.Collections.Generic;
+
+namespace AddressPlan.BL.BusinessServices
+{
+    public sealed class SubdivisionBusinessService : BaseBusinessService
+    {
+        public SubdivisionBusinessService() : base(false)
+        {
+        }
+
+        public IEnumerable<SubdivisionBusinessObject> GetSubdivisions(bool wantNull)
+        {
+            const string query = @"SELECT *
+                                   FROM dbo.Subdivision";
+
+            List<SubdivisionBusinessObject> collection = DataServices.Unit.Subdivisions.GetEntities<SubdivisionBusinessObject>(query);
+
+            if (!wantNull)
+            {
+                return collection;
+            }
+
+            collection = new List<SubdivisionBusinessObject>
+            {
+                new SubdivisionBusinessObject
+                {
+                    SubdivisionId = 0,
+                    SubdivisionName = "Все подразделения"
+                }
+            };
+            collection.AddRange(DataServices.Unit.Subdivisions.GetEntities<SubdivisionBusinessObject>(query));
+            return collection;
+        }
+    }
+}
